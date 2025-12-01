665249899a669df2268b304bfa0b17074abbeeab
vagra348 <vagra348@gmail.com>
2025-12-02 01:16:49 +0300

    Initial commit: project structure

---
+++ backend/tf-backend/src/main/java/tennisfan/tf_backend/TfBackendApplication.java
@@ -0,0 +1,13 @@
+package tennisfan.tf_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TfBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TfBackendApplication.class, args);
+	}
+
+}
---
+++ backend/tf-backend/src/main/java/tennisfan/tf_backend/controllers/MainController.java
@@ -0,0 +1,16 @@
+package tennisfan.tf_backend.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class MainController {
+
+    @GetMapping
+    public String gohome() {
+        return "homePage";
+    }
+
+}
